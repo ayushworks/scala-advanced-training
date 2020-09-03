311d0cc3d8b3154fc18ae5e8cd8b056fc52de0b5
ayush <ayush.mittal@ing.com>
2020-09-03 11:54:41 +0200

    session2

--- src/main/scala/sessions/part1/DarkSugars.scala
+++ src/main/scala/sessions/part1/DarkSugars.scala
@@ -1,5 +1,7 @@
 package sessions.part1
 
+import scala.util.Try
+
 object DarkSugars extends App {
 
   // syntax sugar #1: methods with single param
@@ -15,4 +17,78 @@ object DarkSugars extends App {
   }
 
   println(call2)
+
+  //plenty of examples in scala libraries for this
+
+  //1. Try
+  Try("this code is tried")
+
+  Try{
+    "this code is tried"
+  }
+
+  //2. map
+  List(1,2,3).map(x => x+1)
+
+  List(1,2,3).map {
+    x =>
+      x + 1
+  }
+
+
+  // syntax sugar #2: single abstract method
+  trait Action {
+    def act(x: Int): Int
+  }
+
+  val anInstance: Action = new Action {
+    override def act(x: Int): Int = x + 1
+  }
+
+  val aFunkyInstance: Action = (x: Int) => x + 1
+
+  // example: Runnables
+  val aThread = new Thread(new Runnable {
+    override def run(): Unit = println("hello, Scala")
+  })
+
+  val aSweeterThread = new Thread(() => println("sweet, Scala!"))
+
+  // syntax sugar #4: multi-word method naming
+
+  class Cricketer(name: String) {
+    def `has a batting average of`(avg: Double) = println(s"$name averages $avg")
+  }
+
+  val sachin = new Cricketer("Sachin")
+  sachin `has a batting average of` 55.0
+
+
+  // syntax sugar #5: infix types
+  class Union[A, B]
+  //normal way
+  val union: Union[Int,String] = ???
+
+  // a better way
+  val union1: Int Union String = ???
+
+  class -->[A, B]
+  val towards: Int --> String = ???
+
+  // syntax sugar #6: update() is very special, much like apply()
+  val anArray = Array(1,2,3)
+  anArray(2) = 7  // rewritten to anArray.update(2, 7)
+  // used in mutable collections
+  // remember apply() AND update()!
+
+  // syntax sugar #7: setters for mutable containers
+  class Mutable {
+    private var internalMember: Int = 0 // private for OO encapsulation
+    def member = internalMember // "getter"
+    def member_=(value: Int): Unit =
+      internalMember = value // "setter"
+  }
+
+  val aMutableContainer = new Mutable
+  aMutableContainer.member = 42 // rewrittern as aMutableContainer.member_=(42)
 }
---
+++ src/main/scala/sessions/part1/PatternMatching.scala
@@ -0,0 +1,117 @@
+package sessions.part1
+
+object PatternMatching extends App {
+
+  //recap
+
+  // pattern matching
+  val x = 2
+  val order = x match {
+    case 1 => "first"
+    case 2 => "second"
+    case 3 => "third"
+    case _ => x + "th"
+  }
+
+  case class Human(name: String, age: Int)
+
+  val jim = Human("Jim", 22)
+  val greetings = jim match {
+    case Human(n, _) => s"Hi, my name is $n"
+  }
+
+
+  // Whereas the apply method is like a constructor
+  // which takes arguments and creates an object,
+  // the unapply takes an object
+  // and tries to give back the arguments
+
+  class Person(val name: String, val age: Int)
+
+  object Person {
+
+    //def apply(name: String, age: Int): Person = new  Person(name, age)
+
+    def unapply(person: Person): Option[(String, Int)] =
+      if (person.age < 21) None
+      else Some((person.name, person.age))
+
+    def unapply(age: Int): Option[String] =
+      Some(if (age < 21) "minor" else "major")
+  }
+
+  val bob = new Person("Bob", 20)
+
+  val pair  = Person.unapply(new Person("Bob",21))
+
+  // the type that you match for
+  // is the input to the function
+  // the function must be defined
+  // in the class that you are matching against
+
+  val greeting = bob match {
+    case Person(n, a) => s"Hi, my name is $n and I am $a yo."
+    // case Person(n, a) =>
+    // is a  shorthand syntax for calling
+    // Person.unapply(bob)
+    // which returns the deconstructed values
+    case _ => "no  match"
+
+  }
+
+  println(greeting)
+
+  val legalStatus = bob.age match {
+    case Person(status) => s"My legal status is $status"
+      // case Person(status) is a shorthand for
+      // Person.unapply(age) which returns an Option[String]
+  }
+
+  println(legalStatus)
+
+
+  // custom return types for unapply
+  // isEmpty: Boolean, get: something.
+
+  abstract class Wrapper[T] {
+    def isEmpty: Boolean
+    def get: T
+  }
+
+  object PersonWrapper {
+    def unapply(person: Person): Wrapper[String] = new Wrapper[String] {
+      def isEmpty = false
+      def get= person.name
+    }
+  }
+
+  println(bob match {
+    case PersonWrapper(n) => s"This person's name is $n"
+    case _ => "An alien"
+  })
+
+  // we can also use booleans in return type
+
+  object even {
+    def unapply(arg: Int): Boolean = arg % 2 == 0
+  }
+
+  object singleDigit {
+    def unapply(arg: Int): Boolean = arg > -10 && arg < 10
+  }
+
+  val n: Int = 8
+  val mathProperty = n match {
+    case singleDigit() => "single digit"
+    case even() => "an even number"
+    case _ => "no property"
+  }
+
+  // infix patterns
+  case class Or[A, B](a: A, b: B)
+  val either = Or(2, "two")
+  val humanDescription = either match {
+    case number Or string => s"$number is written as $string"
+  }
+
+}
